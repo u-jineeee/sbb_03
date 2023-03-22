4844b25bfc66f8d7f959e2289fde4f8f2b5bc098
ujineeee <yujin11006@naver.com>
2023-03-22 10:22:46 +0900

    [Add] Question, Answer Repository

---
+++ src/main/java/com/ll/sbb_03/answer/AnswerRepository.java
@@ -0,0 +1,6 @@
+package com.ll.sbb_03.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer,Integer> {
+}
---
+++ src/main/java/com/ll/sbb_03/question/QuestionRepository.java
@@ -0,0 +1,6 @@
+package com.ll.sbb_03.question;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+}
