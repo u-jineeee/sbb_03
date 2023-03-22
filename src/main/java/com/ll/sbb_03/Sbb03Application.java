5c8d9b23f3eed94d8d63432dc1c55f7765189b82
ujineeee <yujin11006@naver.com>
2023-03-22 09:37:40 +0900

    settings

---
+++ src/main/java/com/ll/sbb_03/Sbb03Application.java
@@ -0,0 +1,13 @@
+package com.ll.sbb_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbb03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbb_03/Sbb03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbb_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
