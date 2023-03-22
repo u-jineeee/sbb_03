dbf9859f13a0f97e6754b688f7941892c1071f93
ujineeee <yujin11006@naver.com>
2023-03-22 15:33:31 +0900

    [Add] 질문 등록 구현

--- src/main/java/com/ll/sbb_03/question/QuestionController.java
+++ src/main/java/com/ll/sbb_03/question/QuestionController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -28,5 +29,16 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(){
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(String subject, String content){
+        this.questionService.create(subject, content);
+        return "redirect:/question/list"; //질문 저장 후 질문목록으로 이동
     }
 }
