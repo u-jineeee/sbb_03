65cd248ae83fe19fe1ac6ef5841f9d79144fcdf9
ujineeee <yujin11006@naver.com>
2023-03-23 13:14:00 +0900

    [Add] SecurityConfig

---
+++ src/main/java/com/ll/sbb_03/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.ll.sbb_03;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration //스프링의 환경 설정 파일임을 의미
+@EnableWebSecurity //모든 요청 url이 스프링 시큐리티의 제어를 받도록 만듦.
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers(
+                new AntPathRequestMatcher("/**")).permitAll();
+        return http.build();
+    }
+}
