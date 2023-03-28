d5453ff4368fe66c0e7461d8e6756cb98ca868ca
ujineeee <yujin11006@naver.com>
2023-03-28 14:38:36 +0900

    [Add] 질문 추천 버튼 기능

--- src/main/java/com/ll/sbb_03/question/Question.java
+++ src/main/java/com/ll/sbb_03/question/Question.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -32,4 +33,7 @@ public class Question {
 
     @ManyToOne
     private SiteUser author;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
