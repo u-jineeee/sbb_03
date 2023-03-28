f3bd182b9aba150d2129b370a07acfd879a793f1
ujineeee <yujin11006@naver.com>
2023-03-28 13:38:02 +0900

    [Add] 답변 수정 기능 추가

--- src/main/java/com/ll/sbb_03/answer/AnswerService.java
+++ src/main/java/com/ll/sbb_03/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.ll.sbb_03.answer;
 
+import com.ll.sbb_03.DataNotFoundException;
 import com.ll.sbb_03.question.Question;
 import com.ll.sbb_03.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,6 +21,21 @@ public class AnswerService {
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(author);
+        this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if(answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
 }
