fd635f703c7ae279472cb43f1f92952785539440
ujineeee <yujin11006@naver.com>
2023-03-28 13:15:03 +0900

    [Add] 질문 삭제 기능 추가

--- src/main/java/com/ll/sbb_03/question/QuestionService.java
+++ src/main/java/com/ll/sbb_03/question/QuestionService.java
@@ -50,4 +50,8 @@ public class QuestionService {
         question.setModifyDate(LocalDateTime.now());
         this.questionRepository.save(question);
     }
+
+    public void delete(Question question) {
+        this.questionRepository.delete(question);
+    }
 }
